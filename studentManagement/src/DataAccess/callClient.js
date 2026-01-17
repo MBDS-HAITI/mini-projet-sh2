79ebb85523d560b678f277b61a5b406afc69def0
Samantha Henry <henrysamamntha.pro@gmail.com>
2026-01-17 15:45:08 -0500

    ....

--- studentManagement/src/DataAccess/callClient.js
+++ studentManagement/src/DataAccess/callClient.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 import {getToken} from './Services/authentificationService';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE
+  //baseURL: import.meta.env.VITE_API_BASE
+  baseURL: "https://studentmanagementAPI.shenrys.org/api"
 });
 
 api.interceptors.request.use(
