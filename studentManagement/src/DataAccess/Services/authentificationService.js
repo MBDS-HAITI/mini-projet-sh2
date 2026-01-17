3af3afdfb277af35809081f33aad12613242fcd4
Samantha Henry <henrysamamntha.pro@gmail.com>
2026-01-17 08:50:57 -0500

    ...

--- studentManagement/src/DataAccess/Services/authentificationService.js
+++ studentManagement/src/DataAccess/Services/authentificationService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const API_URL = import.meta.env.VITE_API_BASE;
-
+console.log(API_URL);
 const login = async (email, password) => {
   const response = await axios.post(API_URL + "/login", {
     email,
