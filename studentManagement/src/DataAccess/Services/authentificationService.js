591c3c1a52363505094fc6d5b881412dcdf3c6e6
Samantha Henry <henrysamamntha.pro@gmail.com>
2026-01-17 15:17:26 -0500

    ...

--- studentManagement/src/DataAccess/Services/authentificationService.js
+++ studentManagement/src/DataAccess/Services/authentificationService.js
@@ -3,6 +3,7 @@ const API_URL = import.meta.env.VITE_API_BASE;
 console.log(API_URL);
 const login = async (email, password) => {
   const response = await axios.post("https://studentmanagementAPI.shenrys.org/api/login", {
+  //const response = await axios.post(API_URL+"/login", {
     email,
     password
   });
